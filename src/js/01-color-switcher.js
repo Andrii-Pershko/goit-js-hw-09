ba6bda4c00b295548a235427f9c25c3a2b8c8207
Andrii-Pershko <andriiinf@gmail.com>
2023-02-08 00:21:13 +0200

    1st part complete

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,37 @@
+const refs = {
+    start: document.querySelector("[data-start]"),
+    stop: document.querySelector("[data-stop]"),
+    body: document.querySelector("body"),
+    timerId: null,
+}
 
+refs.stop.disabled = true;
+
+refs.start.addEventListener('click', onPressStart);
+refs.stop.addEventListener('click', onPressStop)
+
+function onPressStart() {
+
+    refs.body.style.backgroundColor = getRandomHexColor();
+    const timerid = setInterval(editBackgroundColor, 1000);
+
+    refs.stop.disabled = false;
+    this.disabled = true;
+    refs.timerId = timerid;
+}
+
+function onPressStop() {
+
+    refs.start.disabled = false;
+    refs.stop.disabled = true;
+
+    clearInterval(refs.timerId);
+}
+
+function editBackgroundColor() {
+    refs.body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
