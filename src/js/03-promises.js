ad2471e4642d2cd3ad674afb44752aae7407623d
Andrii-Pershko <andriiinf@gmail.com>
2023-02-10 14:15:43 +0200

    select

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,45 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+    elForm: document.querySelector('.form'),
+}
+
+refs.elForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const {
+        elements: { delay, step, amount }
+    } = e.currentTarget;
+
+    const Ndelay = Number(delay.value);
+    let Nstep = Ndelay;
+    const Namount = Number(amount.value);
+
+    for (let i = 1; i <= Namount; i++) {
+
+        createPromise(i, Nstep)
+            .then(({ position, delay }) => {
+                Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+            })
+            .catch(({ position, delay }) => {
+                Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+            });
+        Nstep += Number(step.value)
+
+    }
+
+
+
+
+});
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+    const shouldResolve = Math.random() > 0.3;
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (shouldResolve) {
+                resolve({ position, delay })
+            }
+            reject({ position, delay });
+        }, delay)
+    })
 }
